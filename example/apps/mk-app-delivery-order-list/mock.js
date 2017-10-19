2eaf55f73cc5ea480ff8e7feb3f366bf79048f86
ziaochina <ziaochina@gmail.com>
2017-10-19 17:40:09 +0800

    zlj

--- example/apps/mk-app-delivery-order-list/mock.js
+++ example/apps/mk-app-delivery-order-list/mock.js
@@ -6,10 +6,28 @@
 import { fetch } from 'mk-utils'
 import moment from 'moment'
 import utils from 'mk-utils'
+import img1 from './img/img1.png'
+import img2 from './img/img2.jpg'
+
 
 const mockData = fetch.mockData
 
 function initMockData() {
+    //税率
+    if (!mockData.taxRates) {
+        mockData.taxRates = [{
+            id: 17,
+            name: '17%'
+        }, {
+            id: 5,
+            name: '5%'
+        }, {
+            id: 3,
+            name: '3%'
+        }]
+    }
+
+    //票据类型
     if (!mockData.ticketTypes) {
         mockData.ticketTypes = [{
             id: 1,
@@ -20,6 +38,487 @@ function initMockData() {
         }]
     }
 
+    //存货
+    if (!mockData.stocks) {
+        mockData.stocks = []
+
+        for (let i = 1; i < 200; i++) {
+            mockData.stocks.push({
+                id: 1000 + i,
+                code: 1000 + i + '',
+                name: '肉' + i,
+                memo: '肉' + i,
+                category: {
+                    id: 1,
+                    name: '商品'
+                },
+                type: {
+                    id: 101,
+                    code: '00101',
+                    name: '肉类'
+                },
+                meaUnit: {
+                    id: 4,
+                    name: '斤'
+                },
+                spec: '',
+                pricingMode: {
+                    id: 2,
+                    name: '全月平均'
+                },
+                taxRate: {
+                    id: 17,
+                    name: '17%'
+                },
+                multiMea: true,
+                disable: false,
+                units: [{
+                    id: 1,
+                    unit: {
+                        id: 4,
+                        name: '斤'
+                    },
+                    conversionRate: 1,
+                    conversionDesc: '',
+                    isBase: true,
+                    isPu: true,
+                    isSa: true,
+                    isSt: true
+                }],
+                prices: [{
+                    id: 1,
+                    unit: {
+                        id: 4,
+                        name: '斤'
+                    },
+                    lastPuPrice: 10,
+                    referPuPrice: 10,
+                    lastSaPrice: 20,
+                    retailPrice: 20,
+                    firstTradePrice: 11,
+                    secondTradePrice: 12,
+                    thirdTradePrice: 13
+                }],
+                imgs: [{
+                    id: 1,
+                    url: img1
+                }, {
+                    id: 2,
+                    url: img2
+                }]
+            })
+        }
+
+        for (let i = 1; i < 6; i++) {
+            mockData.stocks.push({
+                id: 2000 + i,
+                code: 2000 + i + '',
+                name: '酒' + i,
+                memo: '酒' + i,
+                category: {
+                    id: 1,
+                    name: '商品'
+                },
+                type: {
+                    id: 102,
+                    code: '00102',
+                    name: '酒类'
+                },
+                meaUnit: {
+                    id: 4,
+                    name: '斤'
+                },
+                spec: '',
+                pricingMode: {
+                    id: 2,
+                    name: '全月平均'
+                },
+                taxRate: {
+                    id: 17,
+                    name: '17%'
+                },
+                multiMea: true,
+                disable: false,
+                units: [{
+                    id: 1,
+                    unit: {
+                        id: 4,
+                        name: '斤'
+                    },
+                    conversionRate: 1,
+                    conversionDesc: '',
+                    isBase: true,
+                    isPu: true,
+                    isSa: true,
+                    isSt: true
+                }],
+                prices: [{
+                    id: 1,
+                    unit: {
+                        id: 4,
+                        name: '斤'
+                    },
+                    lastPuPrice: 10,
+                    referPuPrice: 10,
+                    lastSaPrice: 20,
+                    retailPrice: 20,
+                    firstTradePrice: 11,
+                    secondTradePrice: 12,
+                    thirdTradePrice: 13
+                }],
+                imgs: [{
+                    id: 1,
+                    url: img1
+                }, {
+                    id: 2,
+                    url: img2
+                }]
+            })
+        }
+
+        for (let i = 1; i < 10; i++) {
+            mockData.stocks.push({
+                id: 3000 + i,
+                code: 3000 + i + '',
+                name: '干果' + i,
+                memo: '干果' + i,
+                category: {
+                    id: 1,
+                    name: '商品'
+                },
+                type: {
+                    id: 103,
+                    code: '00102',
+                    name: '干果'
+                }
+                ,
+                meaUnit: {
+                    id: 4,
+                    name: '斤'
+                },
+                spec: '',
+                pricingMode: {
+                    id: 2,
+                    name: '全月平均'
+                },
+                taxRate: {
+                    id: 17,
+                    name: '17%'
+                },
+                multiMea: true,
+                disable: false,
+                units: [{
+                    id: 1,
+                    unit: {
+                        id: 4,
+                        name: '斤'
+                    },
+                    conversionRate: 1,
+                    conversionDesc: '',
+                    isBase: true,
+                    isPu: true,
+                    isSa: true,
+                    isSt: true
+                }],
+                prices: [{
+                    id: 1,
+                    unit: {
+                        id: 4,
+                        name: '斤'
+                    },
+                    lastPuPrice: 10,
+                    referPuPrice: 10,
+                    lastSaPrice: 20,
+                    retailPrice: 20,
+                    firstTradePrice: 11,
+                    secondTradePrice: 12,
+                    thirdTradePrice: 13
+                }],
+                imgs: [{
+                    id: 1,
+                    url: img1
+                }, {
+                    id: 2,
+                    url: img2
+                }]
+            })
+        }
+
+        for (let i = 1; i < 34; i++) {
+            mockData.stocks.push({
+                id: 4000 + i,
+                code: 4000 + i + '',
+                name: '衣服' + i,
+                memo: '衣服' + i,
+                category: {
+                    id: 1,
+                    name: '商品'
+                },
+                type: {
+                    id: 201,
+                    code: '00201',
+                    name: '衣服'
+                }
+                ,
+                meaUnit: {
+                    id: 2,
+                    name: '件'
+                },
+                spec: '',
+                pricingMode: {
+                    id: 2,
+                    name: '全月平均'
+                },
+                taxRate: {
+                    id: 17,
+                    name: '17%'
+                },
+                multiMea: true,
+                disable: false,
+                units: [{
+                    id: 1,
+                    unit: {
+                        id: 2,
+                        name: '件'
+                    },
+                    conversionRate: 1,
+                    conversionDesc: '',
+                    isBase: true,
+                    isPu: true,
+                    isSa: true,
+                    isSt: true
+                }],
+                prices: [{
+                    id: 1,
+                    unit: {
+                        id: 2,
+                        name: '件'
+                    },
+                    lastPuPrice: 10,
+                    referPuPrice: 10,
+                    lastSaPrice: 20,
+                    retailPrice: 20,
+                    firstTradePrice: 11,
+                    secondTradePrice: 12,
+                    thirdTradePrice: 13
+                }],
+                imgs: [{
+                    id: 1,
+                    url: img1
+                }, {
+                    id: 2,
+                    url: img2
+                }]
+            })
+        }
+
+        for (let i = 1; i < 8; i++) {
+            mockData.stocks.push({
+                id: 5000 + i,
+                code: 5000 + i + '',
+                name: '鞋子' + i,
+                memo: '鞋子' + i,
+                category: {
+                    id: 1,
+                    name: '商品'
+                },
+                type: {
+                    id: 202,
+                    code: '00202',
+                    name: '鞋子'
+                }
+                ,
+                meaUnit: {
+                    id: 5,
+                    name: '双'
+                },
+                spec: '',
+                pricingMode: {
+                    id: 2,
+                    name: '全月平均'
+                },
+                taxRate: {
+                    id: 17,
+                    name: '17%'
+                },
+                multiMea: true,
+                disable: false,
+                units: [{
+                    id: 1,
+                    unit: {
+                        id: 5,
+                        name: '双'
+                    },
+                    conversionRate: 1,
+                    conversionDesc: '',
+                    isBase: true,
+                    isPu: true,
+                    isSa: true,
+                    isSt: true
+                }],
+                prices: [{
+                    id: 1,
+                    unit: {
+                        id: 5,
+                        name: '双'
+                    },
+                    lastPuPrice: 10,
+                    referPuPrice: 10,
+                    lastSaPrice: 20,
+                    retailPrice: 20,
+                    firstTradePrice: 11,
+                    secondTradePrice: 12,
+                    thirdTradePrice: 13
+                }],
+                imgs: [{
+                    id: 1,
+                    url: img1
+                }, {
+                    id: 2,
+                    url: img2
+                }]
+            })
+        }
+
+        for (let i = 1; i < 28; i++) {
+            mockData.stocks.push({
+                id: 6000 + i,
+                code: 6000 + i + '',
+                name: '饰品' + i,
+                memo: '饰品' + i,
+                category: {
+                    id: 1,
+                    name: '商品'
+                },
+                type: {
+                    id: 203,
+                    code: '00203',
+                    name: '饰品'
+                }
+                ,
+                meaUnit: {
+                    id: 2,
+                    name: '件'
+                },
+                spec: '',
+                pricingMode: {
+                    id: 2,
+                    name: '全月平均'
+                },
+                taxRate: {
+                    id: 17,
+                    name: '17%'
+                },
+                multiMea: true,
+                disable: false,
+                units: [{
+                    id: 1,
+                    unit: {
+                        id: 2,
+                        name: '件'
+                    },
+                    conversionRate: 1,
+                    conversionDesc: '',
+                    isBase: true,
+                    isPu: true,
+                    isSa: true,
+                    isSt: true
+                }],
+                prices: [{
+                    id: 1,
+                    unit: {
+                        id: 2,
+                        name: '件'
+                    },
+                    lastPuPrice: 10,
+                    referPuPrice: 10,
+                    lastSaPrice: 20,
+                    retailPrice: 20,
+                    firstTradePrice: 11,
+                    secondTradePrice: 12,
+                    thirdTradePrice: 13
+                }],
+                imgs: [{
+                    id: 1,
+                    url: img1
+                }, {
+                    id: 2,
+                    url: img2
+                }]
+            })
+        }
+
+        for (let i = 1; i < 50; i++) {
+            mockData.stocks.push({
+                id: 7000 + i,
+                code: 7000 + i + '',
+                name: '设备' + i,
+                memo: '设备' + i,
+                category: {
+                    id: 1,
+                    name: '商品'
+                },
+                type: {
+                    id: 3,
+                    code: '003',
+                    name: '设备',
+                }
+                ,
+                meaUnit: {
+                    id: 3,
+                    name: '台'
+                },
+                spec: '',
+                pricingMode: {
+                    id: 2,
+                    name: '全月平均'
+                },
+                taxRate: {
+                    id: 17,
+                    name: '17%'
+                },
+                multiMea: true,
+                disable: false,
+                units: [{
+                    id: 1,
+                    unit: {
+                        id: 3,
+                        name: '台'
+                    },
+                    conversionRate: 1,
+                    conversionDesc: '',
+                    isBase: true,
+                    isPu: true,
+                    isSa: true,
+                    isSt: true
+                }],
+                prices: [{
+                    id: 1,
+                    unit: {
+                        id: 3,
+                        name: '台'
+                    },
+                    lastPuPrice: 10,
+                    referPuPrice: 10,
+                    lastSaPrice: 20,
+                    retailPrice: 20,
+                    firstTradePrice: 11,
+                    secondTradePrice: 12,
+                    thirdTradePrice: 13
+                }],
+                imgs: [{
+                    id: 1,
+                    url: img1
+                }, {
+                    id: 2,
+                    url: img2
+                }]
+            })
+        }
+    }
+
+
+    //客户
     if (!mockData.customers) {
         mockData.customers = [{
             id: 1,
@@ -30,6 +529,43 @@ function initMockData() {
         }]
     }
 
+    //仓库
+    if (!mockData.warehouses) {
+        mockData.warehouses = [{
+            id: 1,
+            name: '北京仓'
+        }, {
+            id: 2,
+            name: '上海仓'
+        }]
+    }
+
+    //结算方式
+    if (!mockData.settlementModes) {
+        mockData.settlementModes = [{
+            id: 1,
+            name: '现结'
+        }, {
+            id: 2,
+            name: '月结'
+        }]
+    }
+
+    //资金账户
+    if (!mockData.assetAccounts) {
+        mockData.assetAccounts = [{
+            id: 1,
+            name: '现金'
+        }, {
+            id: 2,
+            name: '支付宝'
+        }, {
+            id: 3,
+            name: '微信'
+        }]
+    }
+
+    // 销售出库单
     if (!mockData.deliveryOrders) {
         mockData.deliveryOrders = []
         for (let i = 0; i < 200; i++) {
@@ -37,16 +573,33 @@ function initMockData() {
                 id: i,
                 code: 'do20170101' + (100 + i + 1),
                 ticketType: { id: 1, name: '专用发票' },
+                warehouse: { id: 1, name: '北京仓' },
                 date: `2017-${i % 11 + 1}-${i % 28 + 1}`,
                 customer: { id: 1, name: '腾讯' },
+                salesman: '张三',
                 voucherNO: i % 2 == 1 ? '' : 'VO' + (10000 + i),
                 receiptNumber: i % 2 == 1 ? '' : 'RE' + (10000 + i),
                 amount: 100,
-                priceTaxTotal: 130,
-                paidAmount: i % 2 == 1 ? 80 : 100,
-                unpaidAmount: i % 2 == 1 ? 20 : 0,
+                priceTaxTotal: 105,
+                paidAmount: 105 - (i % 3) * 10,
+                unpaidAmount: (i % 3) * 10,
                 isAudit: i % 3 == 0,
-                memo: ''
+                memo: '备注' + i,
+                settlementMode: { id: 1, name: '现结' },
+                details: [{
+                    id: 1,
+                    stock: mockData.stocks[1],
+                    price: 10,
+                    number: 10,
+                    amount: 100,
+                    taxRate: { id: 5, name: '5%' },
+                    tax: 5,
+                    priceTaxTotal: 105
+                }],
+                settlements: [{
+                    account: { id: 1, name: '现金' },
+                    settlementAmount: 105 - (i % 3) * 10
+                }]
             })
         }
     }
@@ -106,8 +659,8 @@ function query(option) {
             data = data.filter(o => moment(o.date).isBetween(dates[0], dates[1], null, '()'))
         }
 
-        if(filter.customer){
-            data = data.filter(o => o.customer && o.customer.id == filter.customer.id)            
+        if (filter.customer) {
+            data = data.filter(o => o.customer && o.customer.id == filter.customer.id)
         }
 
         if (filter.code)
